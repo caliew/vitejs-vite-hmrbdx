684ca7f5accc8a4c692b23365f8860ab57199e93
caliew <caliew888@gmail.com>
2023-05-30 11:25:18 +0800

    1 file modified

--- src/FetchData.tsx
+++ src/FetchData.tsx
@@ -8,7 +8,7 @@ const FetchData = (props) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts',{ method: 'GET'});
         if (!response.ok) {
           throw new Error('Request failed');
         }
